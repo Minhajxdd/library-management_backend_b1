a0207cff410c48224424490b63df512a438f013e
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 20:10:14 +0530

    feat(cookieParser): registered

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -11,6 +13,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
