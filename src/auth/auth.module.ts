2e6aa1891b20d55333d95a625b776efdc107ce29
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 14:38:09 +0530

    build(auth): formated auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { AuthService } from './Service/auth.service';
+import { AuthController } from './Controller/auth.controller';
 
 @Module({
   controllers: [AuthController],
