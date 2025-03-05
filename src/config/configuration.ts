e06b2af83f3329abce2a4109d005ce296eac5ff4
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 15:19:46 +0530

    feat(configuration): added more configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,4 +3,10 @@ export default () => ({
   database: {
     connectionString: process.env.MONGO_URL,
   },
+  node_environment: {
+    node_env: process.env.NODE_ENV,
+  },
+  domain: {
+    domain_name: process.env.DOMAIN,
+  },
 });
