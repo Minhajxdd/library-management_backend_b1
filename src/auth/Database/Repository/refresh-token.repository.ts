5810429fcf9815710c79e8510dba52da7f4d3b3e
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 15:42:23 +0530

    feat(refresh-token): added interface refresh-toekn-repository

--- src/auth/Database/Repository/refresh-token.repository.ts
+++ src/auth/Database/Repository/refresh-token.repository.ts
@@ -3,9 +3,13 @@ import { GenericRepository } from '../Repository/generic.repository';
 import { RefreshToken } from '../Schemea/refresh-token.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { IRefreshTokenRepository } from 'src/auth/Interface/Repository/refresh-token-repository.interface';
 
 @Injectable()
-export class RefreshTokenRepository extends GenericRepository<RefreshToken> {
+export class RefreshTokenRepository
+  extends GenericRepository<RefreshToken>
+  implements IRefreshTokenRepository
+{
   constructor(
     @InjectModel(RefreshToken.name) private _refreshToken: Model<RefreshToken>,
   ) {
---
+++ src/auth/Interface/Repository/refresh-token-repository.interface.ts
@@ -0,0 +1,6 @@
+import { RefreshToken } from '../../Database/Schemea/refresh-token.schema';
+import { IGenericRepository } from './generic-repository.interface';
+
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+export interface IRefreshTokenRepository
+  extends IGenericRepository<RefreshToken> {}
