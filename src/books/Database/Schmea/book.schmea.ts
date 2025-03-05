94306cb80d199c75032b8bfc86f4dacfac9cac47
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 20:35:01 +0530

    feat(book): added book schmea

---
+++ src/books/Database/Schmea/book.schmea.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type BookDocument = Book & Document;
+
+@Schema({ timestamps: true })
+export class Book {
+  @Prop({ required: true })
+  image: string;
+
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  author: string;
+
+  @Prop()
+  description?: string;
+
+  @Prop({ required: true, default: 0 })
+  quantity: number;
+}
+
+export const BookSchema = SchemaFactory.createForClass(Book);
