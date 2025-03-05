ce075a68efbd8179781cebbf98766929733f7bd8
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 14:51:03 +0530

    feat(user-schmea): register useschmea at auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './Service/auth.service';
 import { AuthController } from './Controller/auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './Database/Schemea/user.schmea';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
