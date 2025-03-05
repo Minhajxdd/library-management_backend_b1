37517c55db3a81dc88b39a3c87993c93896d505d
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 15:50:28 +0530

    feat(token): added utils

---
+++ src/auth/Utils/token.utils.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { v4 as uuidv4 } from 'uuid';
+import { IUserRepository } from '../Interface/Repository/user-repository.interface';
+import { IRefreshTokenRepository } from '../Interface/Repository/refresh-token-repository.interface';
+
+@Injectable()
+export class TokenUtils {
+  constructor(
+    private jwtService: JwtService,
+    @Inject('RefreshTokenRepository')
+    private readonly _refreshTokenRepository: IRefreshTokenRepository,
+    @Inject('UserRepository')
+    private readonly _userRepository: IUserRepository,
+  ) {}
+
+  async GenerateTokens(userId: string): Promise<{
+    accessToken: string;
+    refreshToken: string;
+  }> {
+    const user = await this._userRepository.findById(userId);
+
+    const payload = {
+      userId,
+      isAdmin: user.isAdmin,
+    };
+
+    const accessToken = this.jwtService.sign(payload, { expiresIn: '30m' });
+
+    const refreshToken = uuidv4();
+
+    await this._refreshTokenRepository.updateOneUpsert(refreshToken, userId);
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
