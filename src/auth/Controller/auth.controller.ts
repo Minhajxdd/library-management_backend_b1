6e3acb00906fd6fb8937caf4d88fe3184dc348a6
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 15:58:51 +0530

    feat(auth): register endpoint done

--- src/auth/Controller/auth.controller.ts
+++ src/auth/Controller/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from '../Service/auth.service';
+import { Response } from 'express';
+import { RegisterDto } from '../Dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(registerData: RegisterDto, @Res() res: Response) {
+    return this.authService.register(registerData, res);
+  }
 }
--- src/auth/Service/auth.service.ts
+++ src/auth/Service/auth.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { RegisterDto } from '../Dto/register.dto';
+import { IUserRepository } from '../Interface/Repository/user-repository.interface';
+import { IPasswordUtils } from '../Interface/Utils/password-utils.interface';
+import { ICookieUtils } from '../Interface/Utils/cookie-utils.respository';
+import { ITokenUtils } from '../Interface/Utils/token-utils.repository';
+import { Response } from 'express';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject('UserRepository')
+    private readonly _userRepository: IUserRepository,
+    @Inject('PasswordUtils')
+    private readonly _passwordUtils: IPasswordUtils,
+    @Inject('CookieUtils')
+    private readonly _cookieUtils: ICookieUtils,
+    @Inject('TokenUtils')
+    private readonly _tokenUtils: ITokenUtils,
+  ) {}
+
+  async register(registerData: RegisterDto, res: Response): Promise<void> {
+    const { email, password, fullName } = registerData;
+
+    const emailInUse = await this._userRepository.findOne({ email });
+
+    if (emailInUse) {
+      throw new BadRequestException('Email already in use');
+    }
+
+    const hashPassword = await this._passwordUtils.hashPassword(password);
+
+    const user = await this._userRepository.create({
+      fullName,
+      email,
+      password: hashPassword,
+    });
+
+    const { accessToken, refreshToken } = await this._tokenUtils.GenerateTokens(
+      String(user._id),
+    );
+
+    this._cookieUtils.setCookie(res, [
+      {
+        name: 'refresh_token',
+        value: refreshToken,
+      },
+      {
+        name: 'access_token',
+        value: accessToken,
+        options: { maxAge: 5 * 60 * 1000 },
+      },
+    ]);
+
+    res.json({
+      status: 'sucess',
+      message: 'successfully user Registered',
+    });
+  }
+}
