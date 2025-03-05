7a285764a175cfba855126593ed53e57cd9fa5e8
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 21:06:05 +0530

    feat(books): registerd books schmea

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './Service/books.service';
 import { BooksController } from './Controller/books.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from './Database/Schmea/book.schmea';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Book.name,
+        schema: BookSchema,
+      },
+    ]),
+  ],
   controllers: [BooksController],
   providers: [BooksService],
 })
