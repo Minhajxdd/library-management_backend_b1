046d8d7c8f8a94f722f8a188150a425465442e31
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 23:46:22 +0530

    feat(multer): configured multer

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -4,9 +4,14 @@ import { BooksController } from './Controller/books.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './Database/Schmea/book.schmea';
 import { BooksRepository } from './Database/Repository/books.repository';
+import { MulterModule } from '@nestjs/platform-express';
+import multer from 'multer';
 
 @Module({
   imports: [
+    MulterModule.register({
+      storage: multer?.memoryStorage(),
+    }),
     MongooseModule.forFeature([
       {
         name: Book.name,
