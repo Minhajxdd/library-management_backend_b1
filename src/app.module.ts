63e9a97fd2a7c7161ea5f40f3091cf741be97e24
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 14:50:02 +0530

    feat(config-mongoose): registerd configuraton and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config) => ({
+        uri: config.get('database.connectionString'),
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
