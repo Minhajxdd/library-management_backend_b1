714983459ba5d54bec44751cc632b8e1a86ac053
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 15:38:02 +0530

    fix(refreshtokenrepository); fixed exported class name

--- src/auth/Database/Repository/refresh-token.repository.ts
+++ src/auth/Database/Repository/refresh-token.repository.ts
@@ -5,7 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
-export class UserRepository extends GenericRepository<RefreshToken> {
+export class RefreshTokenRepository extends GenericRepository<RefreshToken> {
   constructor(
     @InjectModel(RefreshToken.name) private _refreshToken: Model<RefreshToken>,
   ) {
