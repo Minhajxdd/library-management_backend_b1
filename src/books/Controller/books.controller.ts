176cd1a85d6e74902454590f95d6c6efed776bcd
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-05 21:35:01 +0530

    feat(books): findall books endpoint doe

--- src/books/Controller/books.controller.ts
+++ src/books/Controller/books.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { BooksService } from '../Service/books.service';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  findAll(
+    @Query() query: any,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+  ) {
+    return this.booksService.findAll(query, Number(page), Number(limit));
+  }
 }
--- src/books/Service/books.service.ts
+++ src/books/Service/books.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { IBooksRepository } from '../Interface/Repository/books-repository.interface';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(
+    @Inject('BooksRepository')
+    private readonly _booksRepository: IBooksRepository,
+  ) {}
+
+  findAll(query: any, page = 1, limit = 10) {
+    return this._booksRepository.findAll(query, page, limit);
+  }
+}
