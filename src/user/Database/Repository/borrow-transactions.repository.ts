792b6ef30e22e8adce061962369a228e7dff4d52
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-06 21:59:54 +0530

    feat(user): borrow transaction interface added

--- src/user/Database/Repository/borrow-transactions.repository.ts
+++ src/user/Database/Repository/borrow-transactions.repository.ts
@@ -3,9 +3,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { GenericRepository } from './generic.repository';
 import { BorrowTransaction } from '../Schmea/borrow-transactions.schema';
+import { IBorrowTransactionRepository } from '../../Interface/Database/Repository/book-transaction-repository.interface';
 
 @Injectable()
-export class BooksRepository extends GenericRepository<BorrowTransaction> {
+export class BorrowTransactionRepository
+  extends GenericRepository<BorrowTransaction>
+  implements IBorrowTransactionRepository
+{
   constructor(
     @InjectModel(BorrowTransaction.name)
     private _borrowTransactionModel: Model<BorrowTransaction>,
--- src/user/Interface/Database/Repository/book-transaction-repository.interface.ts
+++ src/user/Interface/Database/Repository/book-transaction-repository.interface.ts
@@ -1,18 +1,6 @@
+import { BorrowTransaction } from '../../../Database/Schmea/borrow-transactions.schema';
+import { IGenericRepository } from './generic-repository.interface';
 
 /* eslint-disable @typescript-eslint/no-empty-object-type */
-xport interface IBooksRepository extends IGenericRepository<Book> {
-  findAll(
-    query: any,
-    page: number,
-    limit: number,
-  ): Promise<{ data: Book[]; total: number }>;
-
-  getImageAndUpdate(
-    id: string,
-    image: string,
-  ): Promise<{
-    previousProfile: string;
-  }>;
-
-  toggleBlock(book: Book): Promise<void>;
-}
+export interface IBorrowTransactionRepository
+  extends IGenericRepository<BorrowTransaction> {}
