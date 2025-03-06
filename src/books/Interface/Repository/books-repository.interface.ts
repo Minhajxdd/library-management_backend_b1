bbf2fcfb3120fe13bad12040ba2ee0d0d5f004ee
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-06 21:53:08 +0530

    fix(books): removed unwanted ts declaration comment

--- src/books/Interface/Repository/books-repository.interface.ts
+++ src/books/Interface/Repository/books-repository.interface.ts
@@ -1,7 +1,6 @@
 import { Book } from '../../Database/Schmea/book.schmea';
 import { IGenericRepository } from './generic-repository.interface';
 
-/* eslint-disable @typescript-eslint/no-empty-object-type */
 export interface IBooksRepository extends IGenericRepository<Book> {
   findAll(
     query: any,
