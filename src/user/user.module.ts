f8745708b064efe54ce2e4e6977bb947847c53a3
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-07 09:47:51 +0530

    feat(user): registered repository as well as schmea in module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
 import { BorrowTransactionService } from './borrow-transaction.service';
 import { UserBorrowController } from './Controller/user.transaction.controller';
+import { BorrowTransactionRepository } from './Database/Repository/borrow-transactions.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  BorrowTransaction,
+  BorrowTransactionSchema,
+} from './Database/Schmea/borrow-transactions.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: BorrowTransaction.name,
+        schema: BorrowTransactionSchema,
+      },
+    ]),
+  ],
   controllers: [UserBorrowController],
-  providers: [BorrowTransactionService],
+  providers: [
+    BorrowTransactionService,
+    {
+      provide: 'BorrowTransactionRepository',
+      useClass: BorrowTransactionRepository,
+    },
+  ],
 })
 export class UserModule {}
