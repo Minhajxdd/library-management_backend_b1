855c549f8c5c5485425e9e178b319f533c82a7c4
Mohammed Minhaj <nijulinux@gmail.com>
2025-03-10 11:48:07 +0530

    fix(cookie): added domain

--- src/auth/Utils/cookie.utils.ts
+++ src/auth/Utils/cookie.utils.ts
@@ -11,6 +11,7 @@ export class CookieUtils implements ICookieUtils {
   ): void {
     const isProduction =
       configuration().node_environment.node_env === 'production';
+    const domain = configuration().domain.domain_name;
 
     const cookieHeaders = cookies.map(({ name, value, options }) => {
       let option = `${name}=${value};`;
@@ -31,6 +32,10 @@ export class CookieUtils implements ICookieUtils {
         option += 'Secure;';
       }
 
+      if (domain) {
+        option += `Domain=${domain};`;
+      }
+
       return option;
     });
 
@@ -40,6 +45,7 @@ export class CookieUtils implements ICookieUtils {
   clearCookies(res: Response, cookieNames: string[]): void {
     const isProduction =
       configuration().node_environment.node_env === 'production';
+    const domain = configuration().domain.domain_name;
 
     const cookieHeaders = cookieNames.map((name) => {
       let option = `${name}=; Path=/; Max-Age=0;`;
@@ -48,6 +54,10 @@ export class CookieUtils implements ICookieUtils {
 
       if (isProduction) {
         option += 'Secure;';
+      }
+
+      if (domain) {
+        option += `Domain=${domain};`;
       }
 
       return option;
